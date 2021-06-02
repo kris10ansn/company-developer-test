bfee64172986d8183dfea0581e468eb853132ce0
kris10ansn <telefon.kristian@gmail.com>
2021-06-02 17:52:00 +0200

    update

--- maksimer_developer_test.php
+++ maksimer_developer_test.php
@@ -9,57 +9,85 @@
  * Author URI: http://github.com/kris10ansn
 */
 
-
-class PimsEvent
-{
-    public int $id;
-    public int $venue_id;
-    public int $series_id;
-    public string $label;
-    public string $datetime;
-    public string $currency;
-    public int $costing_capacity;
-    public string $sold_out_date;
-}
-
-function debug()
+function debug(): void
 {
     ini_set('display_errors', 1);
     ini_set('display_startup_errors', 1);
     error_reporting(E_ALL);
 }
 
-function pims_api_get($query)
+function basic_auth(string $username, string $password): string
 {
-    $base_url = "https://sandbox.pims.io/api/v1/";
-    $username = "sandbox";
-    $password = "c5jI1ABi8d0x87oWfVzvXALqkf0hToGq";
-
-    $arguments = [
-        "headers" => [
-            "Authorization" => "Basic " . base64_encode("$username:$password")
-        ]
-    ];
-
-    $response = wp_remote_get($base_url . $query, $arguments);
-
-    $response["body"] = json_decode($response["body"]);
-
-    return $response;
+    return "Basic " . base64_encode("$username:$password");
 }
 
-function shortcode_events()
+function pims_get(string $query): object
+{
+    $response = wp_remote_get("https://sandbox.pims.io/api/v1/" . $query, [
+        "headers" => [
+            "Authorization" => basic_auth("sandbox", "c5jI1ABi8d0x87oWfVzvXALqkf0hToGq")
+        ]
+    ]);
+
+    return json_decode($response["body"]);
+}
+
+/**
+ * @param object $pims_response
+ * @return PimsEvent[]
+ */
+function pims_embedded_events(object $pims_response): array
+{
+    $events = $pims_response->_embedded->events;
+
+    return array_map(fn ($event) => new PimsEvent($event), $events);
+}
+
+
+abstract class APIObject {
+    public function __construct(object $object)
+    {
+        foreach ($object as $key => $value) {
+            $this->{$key} = $value;
+        }
+    }
+}
+
+class PimsVenue extends APIObject
+{
+    public int $id;
+    public string $label;
+    public string $city;
+    public string $country_code;
+}
+
+class PimsEvent extends APIObject
+{
+    public int $id;
+    public int $venue_id;
+    public ?int $series_id;
+    public string $label;
+    public string $datetime;
+    public string $currency;
+    public int $costing_capacity;
+    public ?string $sold_out_date;
+
+    public function get_venue(): PimsVenue
+    {
+        $venue_response = pims_get("venues/$this->venue_id");
+        return new PimsVenue($venue_response);
+    }
+}
+
+function shortcode_events(): string
 {
     // Display all errors (for debugging, probably remove in production)
     debug();
 
-    $response = pims_api_get("events");
+    $events_response = pims_get("events");
+    $events = pims_embedded_events($events_response);
 
-    echo "<pre>";
-    var_dump($response["body"]->_embedded->events[0]);
-    echo "</pre>";
-
-    return "<p>.</>";
+    return "<p>.</p>";
 }
 
 add_shortcode("events", "shortcode_events");
